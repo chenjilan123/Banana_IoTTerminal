48acbee7cb6c4d50ea0cd90b5442a5ceeead7ad9
济澜 陈 <357592895@qq.com>
2018-08-24 00:07:06 +0800

    Order Manage

---
+++ IoTTerminal/IoTTerminal.Car/Base/OrderManageSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTTerminal.Car.Base
+{
+    public class OrderManageSystem
+    {
+        //Concurrent Safety?
+        /// <summary>
+        /// OrderID - MessageID
+        /// </summary>
+        private Dictionary<ushort, ushort> dicWaitRespondOrder;
+        public OrderManageSystem()
+        {
+            this.dicWaitRespondOrder = new Dictionary<ushort, ushort>();
+        }
+        public void AddOrder(ushort orderID, ushort messageID)
+        {
+            dicWaitRespondOrder.Add(orderID, messageID);
+        }
+
+        public bool IsResponse(ushort responseOrderID, ushort messageID)
+        {
+            if (!dicWaitRespondOrder.ContainsKey(responseOrderID) || dicWaitRespondOrder[responseOrderID] != messageID)
+                return false;
+            dicWaitRespondOrder.Remove(responseOrderID);
+            return true;
+        }
+    }
+}
